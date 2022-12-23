5188ad8039de62aab63987a87af5f6636e48542e
Kirsten Opstad <kirsten.opstad@gmail.com>
2022-12-23 09:06:16 -0800

    add client class

---
+++ HairSalon/Models/Client.cs
@@ -0,0 +1,15 @@
+namespace HairSalon.Models
+{
+  public class Client
+  {
+    // properties
+    public int ClientId {get; set;}
+    public string FirstName {get; set;}
+    public string LastName {get; set;}
+    public string Email {get; set;}
+    // foreign key
+    public int StylistId {get; set;}
+    // reference navigation property
+    public Stylist Stylist {get; set;}
+  }
+}
