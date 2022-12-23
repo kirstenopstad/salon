cf156da9cde7e5cd262e87e04e56c9f5dc802536
Kirsten Opstad <kirsten.opstad@gmail.com>
2022-12-23 10:50:33 -0800

    add index and create (get) route

---
+++ HairSalon/Controllers/ClientsController.cs
@@ -0,0 +1,37 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using HairSalon.Models;
+
+namespace HairSalon.Controllers
+{
+  public class ClientsController : Controller
+  {
+    // get db context
+    private readonly HairSalonContext _db;
+
+    // constructor for controller
+    public ClientsController(HairSalonContext db)
+    {
+      // copies db to private controller copy of db
+      _db = db;
+    }
+
+    // routes
+    public ActionResult Index()
+    {
+      List<Client> model = _db.Clients.ToList();
+      return View(model);
+    }
+
+    public ActionResult Create()
+    {
+      // add viewbag to pass in select options
+      // SelectList(data that will populate <option> elements, value, displayed text)
+      ViewBag.StylistId = new SelectList(_db.Stylists, "StylistID", "Name") ;
+      return View();
+    }
+  }
+}
