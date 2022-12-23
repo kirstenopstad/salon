a5766061324f0321da192622603c242eb38351b0
Kirsten Opstad <kirsten.opstad@gmail.com>
2022-12-23 09:19:47 -0800

    connect to db + add index route

---
+++ HairSalon/Controllers/StylistsController.cs
@@ -0,0 +1,29 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using HairSalon.Models;
+
+namespace HairSalon.Controllers
+{
+  public class StylistsController : Controller
+  {
+    // get db context
+    private readonly HairSalonContext _db;
+
+    // constructor for controller
+    public StylistsController(HairSalonContext db)
+    {
+      // copies db to private controller copy of db
+      _db = db;
+    }
+
+    // routes
+    public ActionResult Index()
+    {
+      List<Stylist> model = _db.Stylists.ToList();
+      return View(model)
+    }
+  }
+}
