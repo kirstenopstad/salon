4f588039f68cc26c1fbf11d321d1f6c567a61598
Kirsten Opstad <kirsten.opstad@gmail.com>
2022-12-23 16:22:18 -0800

    add properties and reference nav for stylist & client

---
+++ HairSalon/Models/Appointment.cs
@@ -0,0 +1,15 @@
+namespace HairSalon.Models
+{
+  public class Appointment
+  {
+    // properties
+    public int ApptId {get; set;}
+    public string DateTime { get; set;}
+    // foreign keys
+    public int StylistId {get; set;}
+    public int ClientId {get; set;}
+    // reference navigation properties
+    public Stylist Stylist {get; set;}
+    public Client Client {get; set;}
+  }
+}
